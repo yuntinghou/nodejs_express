101fbfbec105f5daa6d3c16118fea572052a4329
yuntinghou <yunting.leo@gmail.com>
2017-08-01 23:24:22 -0700

    Updated the route and home page

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+//New Code
+//var mongo = require('mongodb');
+//var monk = require('monk');
+//var db = monk('localhost:27017/nodetest1');
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 
